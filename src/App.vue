df5e65338a2e5d0a80f6a6cb08003bef975f2251
Hacker God <jyrone.parker@gmail.com>
2025-04-09 21:43:34 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import PWABadge from './components/PWABadge.vue'
+if ("launchQueue" in window) {
+  launchQueue.setConsumer(async (launchParams) => {
+    const file = launchParams.files[0]; 
+    const url = URL.createObjectURL(await file.getFile());
+    console.log(url);
+    }
+  });
+}
+</script>
+
+<template>
+  <div>
+   <img src="/favicon.svg" class="logo" alt="iptv-viewer logo" />
+   
+  </div>
+  <HelloWorld msg="iptv-viewer" />
+  <PWABadge />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'iptv-viewer',
+      short_name: 'IPTV',
+      description: 'A standalone IPTV PWA viewer. Load .m3u8 playlists and watch.',
+      theme_color: '#000000',
+      display: 'standalone',
+      file_handlers: [
+        {
+          action: "/",
+          accept: {
+            "application/x-mpegURL": [".m3u8", ".m3u"]
+          }
+        }
+      ],
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
