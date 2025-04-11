22b2c2178b3cdca1d01dbd285f6cfae85de7a205
Hacker God <jyrone.parker@gmail.com>
2025-04-11 01:25:28 -0400

    scope

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
       "protocol_handlers": [
         {
           "protocol": "web+shaketv",
-          "url": "/?url=%s"
+          "url": "/iptv-viewer?url=%s/"
         }
       ],
       file_handlers: [
