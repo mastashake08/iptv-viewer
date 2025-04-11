828367d5a96cdb1ea57e8d994d043b3fe8c1b2ce
Hacker God <jyrone.parker@gmail.com>
2025-04-10 20:13:58 -0400

    map

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ const parseManifest = (manifest) => {
        poster: '/iptv-viewer/favicon.svg'
      }));
     }
-    sources = sources.filter((source) => source.sources.filter((source) => source.src.startsWith("https://")));
+    sources = sources.map((source) => source.sources.filter((source) => source.src.startsWith("https://")));
     console.log(sources);
     return sources;
 };
