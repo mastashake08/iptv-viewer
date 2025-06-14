162fae016741dac1327bbe48a2f411e6a56b4c58
Jyrone Parker <jyrone.parker@gmail.com>
2025-06-14 06:44:23 -0400

    match

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ const parseManifest = (manifest) => {
       src: segment.uri,
       type: "application/x-mpegURL",
      }], 
-      name: segment.title.match(/group-title="([^"]+)"/) ? match[1] : null,
+      name: segment.title.match(/group-title="([^"]+)"/) ?? null,
       poster: '/favicon.svg'
     }));
    if (sources.length === 0) {
@@ -69,7 +69,7 @@ const parseManifest = (manifest) => {
         src: segment.uri,
         type: "application/x-mpegURL",
       }], 
-       name: playlist.title.match(/group-title="([^"]+)"/) ? match[1] : null,
+       name: playlist.title.match(/group-title="([^"]+)"/) ?? null,
        poster: '/favicon.svg'
      }));
     }
